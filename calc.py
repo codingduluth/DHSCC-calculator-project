b6af6a83f16fdd2bd3877b1c92862f42f2eb1dbc
Uzziel Gonzalez <uzzielgonzalez247@gmail.com>
2024-09-10 15:23:42 -0400

    Version 1.0.0

---
+++ calc.py
@@ -0,0 +1,40 @@
+# Create a calculator program that takes two numbers and takes an operation and applies it to the two numbers. 
+
+# num1 
+# num2 
+# operators 
+# 1 (Addition)
+# 2 (Subtraction) 
+# 3 (Multiplication) 
+# 4 (Division) 
+
+# Use if else statements to decide which operator to use. 
+
+# Print out the answer to the terminal.
+
+#USEFUL THINGS TO KNOW 
+#float() converts value into a decimal number`                                                                                                                              `
+#input("")
+
+# Calculator code goes here: 
+
+num1 = float(input("What is num1? "))
+num2 = float(input("What is num2? "))
+operator = float(input("What operation do you want to do?\n 1 (Addition) \n 2 (Subtraction) \n 3 (Multiplication) \n 4 (Division) \n"))
+
+if operator == 1:
+    print(num1 + num2)
+elif operator == 2: 
+    print (num1 - num2)
+elif operator == 3: 
+    print(num1 * num2)
+elif operator == 4: 
+    if num2 != 0: 
+        print(num1 / num2)
+    else: 
+        print("Error: Division by 0")
+else: 
+    print("Invalid Operator Selected")
+
+
+    
